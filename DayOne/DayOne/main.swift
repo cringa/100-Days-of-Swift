6df7d780d34724f9ae3554a9388bb0f04b5c28f4
Inga <inga.curjos@gmail.com>
2020-12-08 16:46:12 +0200

    Simple types

---
+++ DayOne/DayOne/main.swift
@@ -0,0 +1,47 @@
+//
+//  main.swift
+//  DayOne
+//
+//  Created by Inga Curjos on 08.12.2020.
+//
+
+import Foundation
+
+// 1. Variables
+var srt = "Hello, World!"
+var str = "Goodbye!"
+
+// 2. Strings and integers
+var age = 38
+var population = 8_000_000
+
+// 3. Multi-line strings
+var srt1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// 4. Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+// 5. String interpolation
+var score = 85
+var str5 = "Your score was \(score)"
+var results = "The test results are here: \(str5)"
+
+// 6. Constants
+let taylor = "swift"
+
+// 7. Type annotations
+let str6 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
