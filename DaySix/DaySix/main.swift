4837d69371db4667356ea643a47f5f271bad5ede
Inga <inga.curjos@gmail.com>
2020-12-14 12:33:04 +0200

    Closures, part one

---
+++ DaySix/DaySix/main.swift
@@ -0,0 +1,54 @@
+//
+//  main.swift
+//  DaySix
+//
+//  Created by Inga Curjos on 14.12.2020.
+//
+
+import Foundation
+
+// 1. Creating basic closures
+let driving = {
+    print("I'm driving in my car")
+}
+driving()
+
+// 2. Accepting parameters in a closure
+let driving2 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+driving2("London")
+
+// 3. Returning values from a closure
+let driving3 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+let message = drivingWithReturn("London")
+print(message)
+
+// 4. Closures as parameters
+let driving4 = {
+    print("I'm driving in my car")
+}
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving4)
+
+// 5. Trailing closure syntax
+func travel1(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel1() {
+    print("I'm driving in my car")
+}
+travel1 {
+    print("I'm driving in my car")
+}
